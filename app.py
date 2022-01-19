d902fafc720fe51fd8ed859cfc75419ce8ecbd36
Bruno Galletti Souza <94149500+brunogalletti@users.noreply.github.com>
2022-01-18 22:54:36 -0300

    Test Fail Pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Bruno Galletti"
+    return "Hello World - Bruno Galletti 2"
 
 if __name__ == '__main__':
     app.run()
