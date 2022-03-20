e80b4a73007aba328829afc46a16755bd116445c
Bruno Galletti Souza <94149500+brunogalletti@users.noreply.github.com>
2022-03-20 17:47:01 -0300

    Alterado text saida

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Bruno Galletti"
+    return "Hello World - Bruno Galletti - G1"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
